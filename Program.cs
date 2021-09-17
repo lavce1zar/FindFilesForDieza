209b6e65d216bd21094e99355cb46819a05a2a73
Andrei Liahotsin <lav.ce1zar@gmail.com>
2021-09-17 12:27:30 +0300

    Add files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindFilesForDieza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Для применения программы необходимо ввести разрешение искомых файлов. Пример: " + '.' + "exe");
+                return;
+            }
+
+            if (args[0][0] != '.')
+            {
+                Console.WriteLine("Введено некорректное разрешение искомых файлов. Разрешение должно начинаться с .");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Введеное разрешение искомых файлов: " + args[0]);
+            }
+            
+
+
+        }
+    }
+}
